e857b9e00d11d0e52a6352b61f077e899184f6aa
jheng.wei0919@gmail.com <JackPan@jackpan-MacBook-Pro.local>
2019-02-02 12:51:42 +0800

    first commit

---
+++ stockcomputer/stockcomputer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stockcomputer
+//
+//  Created by  JackPan on 2019/2/2.
+//  Copyright © 2019 jackpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
