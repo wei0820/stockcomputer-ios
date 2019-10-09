b962199587fe9edf2e8881adcbeabfe4f0fe2c67
Wei Wei <jheng.wei0919@gmail.com>
2019-10-09 10:12:49 +0800

    add address to lat lon

--- stockcomputer/stockcomputer/MapManager.swift
+++ stockcomputer/stockcomputer/MapManager.swift
@@ -9,19 +9,16 @@
 import Foundation
 import MapKit
 class MapManager{
-    func geocode(latitude: Double, longitude: Double, completion: @escaping (CLPlacemark?, Error?) -> ())  {
-           //1
-           let locale = Locale(identifier: "zh_TW")
-           let loc: CLLocation = CLLocation(latitude: latitude, longitude: longitude)
-           if #available(iOS 11.0, *) {
-               CLGeocoder().reverseGeocodeLocation(loc, preferredLocale: locale) { placemarks, error in
-               guard let placemark = placemarks?.first, error == nil else {
-                   UserDefaults.standard.removeObject(forKey: "AppleLanguages")
-                   completion(nil, error)
-                   return
-               }
-               completion(placemark, nil)
-           }
-       }
+//地址轉經緯度(geocodeAddressString)
+    static func AddressToLatLon(s:String){
+    let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString((s), completionHandler: {(placemarks, error) -> Void in
+
+            if let placemark = placemarks?.first {
+                let coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+                coordinates.latitude
+                coordinates.longitude
+            }
+        })
 }
 }
