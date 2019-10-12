13223ecdaa7182a6c314e763e327435a161b873c
wei0820 <jheng.wei0919@gmail.com>
2019-10-12 12:22:29 +0800

    fix error

--- stockcomputer/stockcomputer/MapViewController.swift
+++ stockcomputer/stockcomputer/MapViewController.swift
@@ -11,12 +11,10 @@ import Mapbox
 class MapViewController: UIViewController,MGLMapViewDelegate{
     let userDefaults = UserDefaults.standard
     @IBAction func nowlocation(_ sender: Any) {
-        mapview.setCenter((mapview.userLocation?.coordinate)!, animated: false)
-               var lat  : Double = mapview.userLocation?.coordinate.latitude ?? 0.0
-               var lon :Double =   mapview.userLocation?.coordinate.longitude ?? 0.0
-
-               mapview.setCenter(CLLocationCoordinate2DMake(lat, lon), animated: false)
-                    mapview.zoomLevel = 15
+        var lat  : Double = (mapview.userLocation?.coordinate.latitude)!
+        var lon :Double =   (mapview.userLocation?.coordinate.longitude)!
+        mapview.setCenter(CLLocationCoordinate2DMake(lat, lon), animated: false)
+        mapview.zoomLevel = 15
     }
     
     @IBOutlet var mapview: MGLMapView!
@@ -24,69 +22,65 @@ class MapViewController: UIViewController,MGLMapViewDelegate{
         super.viewDidLoad()
         
         mapview.setCenter(CLLocationCoordinate2DMake(25.034815, 121.564392), animated: false)
-             mapview.zoomLevel = 15
+        mapview.zoomLevel = 15
         mapview.styleURL = MGLStyle.streetsStyleURL
         mapview.delegate = self
-             
-             // Allow the map view to display the user's location
-             mapview.showsUserLocation = true
-        // Do any additional setup after loading the view.
-        setRightButton(s: "加入")
-    }
-     func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
-           mapView.setCenter((mapView.userLocation?.coordinate)!, animated: false)
-       }
-     
-       func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
-           let camera = MGLMapCamera(lookingAtCenter: mapView.centerCoordinate, altitude: 4500, pitch: 15, heading: 180)
-           
-           // Animate the camera movement over 5 seconds.
-           mapView.setCamera(camera, withDuration: 5, animationTimingFunction: CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut))
-       }
-       
-       /*
-        // MARK: - Navigation
-        
-        // In a storyboard-based application, you will often want to do a little preparation before navigation
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        }
-        */
-       func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
-           style.localizeLabels(into: nil)
-           // Into Simplified Chinese
-           style.localizeLabels(into: Locale(identifier: "zh-Tw"))
-           // Into the local language where a given feature is located
-           style.localizeLabels(into: Locale(identifier: "mul"))
-     
-       }
-    /*
-    // MARK: - Navigation
+        mapview.showsUserLocation = true
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //        setRightButton(s: "加入")
     }
-    */
-     func setRightButton(s: String){
-            // 導覽列右邊按鈕
-            
-            let rightButton = UIBarButtonItem(
-                title:s,
-                style:.plain,
-                target:self,
-                action:#selector(ViewController.setting))
-            // 加到導覽列中
-            self.navigationItem.rightBarButtonItem = rightButton
-            
-        }
-        @objc func setting() {
-            
-//            setData()
-            
-        }
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+        mapView.setCenter((mapView.userLocation?.coordinate)!, animated: false)
+    }
+    
+    func mapView(_ mapView: MGLMapView, didSelect annotation: MGLAnnotation) {
+        let camera = MGLMapCamera(lookingAtCenter: mapView.centerCoordinate, altitude: 4500, pitch: 15, heading: 180)
+        
+        // Animate the camera movement over 5 seconds.
+        mapView.setCamera(camera, withDuration: 5, animationTimingFunction: CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut))
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        // Into Simplified Chinese
+        style.localizeLabels(into: Locale(identifier: "zh-Tw"))
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    func setRightButton(s: String){
+        // 導覽列右邊按鈕
+        
+        let rightButton = UIBarButtonItem(
+            title:s,
+            style:.plain,
+            target:self,
+            action:#selector(ViewController.setting))
+        // 加到導覽列中
+        self.navigationItem.rightBarButtonItem = rightButton
+        
+    }
+    @objc func setting() {
+        
+        //            setData()
+        
+        
+        
+    }
     func setData(){
         let controller = UIAlertController(title: "加入", message: "請輸入您要推薦的營業員資料", preferredStyle: .alert)
         controller.addTextField { (textField) in
@@ -94,18 +88,18 @@ class MapViewController: UIViewController,MGLMapViewDelegate{
             textField.keyboardType = UIKeyboardType.default
         }
         controller.addTextField { (textField) in
-           textField.placeholder = "電話"
-           textField.keyboardType = UIKeyboardType.phonePad
+            textField.placeholder = "電話"
+            textField.keyboardType = UIKeyboardType.phonePad
         }
         controller.addTextField { (textField) in
-           textField.placeholder = "地址"
-           textField.keyboardType = UIKeyboardType.default
+            textField.placeholder = "地址"
+            textField.keyboardType = UIKeyboardType.default
         }
         let okAction = UIAlertAction(title: "OK", style: .default) { (_) in
-           let name = controller.textFields?[0].text
-           let phone = controller.textFields?[1].text
+            let name = controller.textFields?[0].text
+            let phone = controller.textFields?[1].text
             let add = controller.textFields?[2].text
-                    
+            
             MapManager.AddressToLatLon(s: add!)
         }
         controller.addAction(okAction)
