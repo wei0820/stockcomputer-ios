e88a9fcae01d326be66e84e1ad72a425d2b88a1f
Wei Wei <jheng.wei0919@gmail.com>
2019-10-17 17:39:56 +0800

    add class

---
+++ stockcomputer/stockcomputer/CalculationManager.swift
@@ -0,0 +1,22 @@
+//
+//  CalculationManager.swift
+//  stockcomputer
+//
+//  Created by oneplay on 2019/10/17.
+//  Copyright © 2019 jackpan. All rights reserved.
+//
+
+import Foundation
+
+class CalculationManager
+{
+    
+    static func getPrice(){
+        
+        
+    }
+    
+    
+    
+    
+}
