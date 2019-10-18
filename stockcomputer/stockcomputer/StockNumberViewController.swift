7a6fc5541ffc52bff32d7b1dd4b90c52071a8d2c
Wei Wei <jheng.wei0919@gmail.com>
2019-10-18 16:04:11 +0800

    add viewcointroller

---
+++ stockcomputer/stockcomputer/StockNumberViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StockNumberViewController.swift
+//  stockcomputer
+//
+//  Created by oneplay on 2019/10/18.
+//  Copyright © 2019 jackpan. All rights reserved.
+//
+
+import UIKit
+
+class StockNumberViewController: MGoogleADViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
