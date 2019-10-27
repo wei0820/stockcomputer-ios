269dbd9168b15b614c9fdb7db35c2e1815e1516c
wei0820 <jheng.wei0919@gmail.com>
2019-10-27 19:19:22 +0800

    fix view

--- stockcomputer/stockcomputer/NowStockPriceViewController.swift
+++ stockcomputer/stockcomputer/NowStockPriceViewController.swift
@@ -20,7 +20,8 @@ class NowStockPriceViewController: MGoogleADViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var time: UILabel!
+    
     /*
     // MARK: - Navigation
 
@@ -30,18 +31,31 @@ class NowStockPriceViewController: MGoogleADViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBOutlet weak var no1: UILabel!
     func getNow(){
         print("========")
-
+        //
         Alamofire.request("https://www.wantgoo.com").responseString { response in
             if let html = response.result.value {
                 if let doc = try? Kanna.HTML(html: html, encoding: String.Encoding.utf8) {
                     for rate in doc.xpath("//*[@id='tabRT1']/table/tbody/tr[1]/td[3]") {
                         print("========")
                         print(rate.text)
+                        self.no1.text = rate.text!
 
                     }
-                }                         }
+                    
+                    for rate in doc.xpath("//*[@id='mainCol']/div[1]/div[1]/span/time") {
+                                        print("========")
+                                        print(rate.text)
+                        self.time.text = rate.text!
+                                    }
+                }
+         
+                
+                
+                
+            }
         }
         
     }
