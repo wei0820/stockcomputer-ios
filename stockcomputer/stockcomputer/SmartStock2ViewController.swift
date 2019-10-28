f9f36dd2c8642569de527e47ee4918501e1ccfaf
Wei Wei <jheng.wei0919@gmail.com>
2019-10-28 15:32:12 +0800

    add view

--- stockcomputer/stockcomputer/SmartStock2ViewController.swift
+++ stockcomputer/stockcomputer/SmartStock2ViewController.swift
@@ -1,18 +1,18 @@
 //
-//  SmartStockViewController.swift
+//  SmartStock2ViewController.swift
 //  stockcomputer
 //
-//  Created by oneplay on 2019/10/25.
+//  Created by oneplay on 2019/10/28.
 //  Copyright © 2019 jackpan. All rights reserved.
 //
 
 import UIKit
 
-class SmartStockViewController: MGoogleADViewController {
+class SmartStock2ViewController: MGoogleADViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "智慧選股-多方股"
+        self.title = "智慧選股-空方股"
 
         // Do any additional setup after loading the view.
     }
