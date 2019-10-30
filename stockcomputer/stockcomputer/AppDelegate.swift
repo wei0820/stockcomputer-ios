43608080016079fb353910798cce48b2848ce6ef
Wei Wei <jheng.wei0919@gmail.com>
2019-10-30 13:37:03 +0800

    add fun

--- stockcomputer/stockcomputer/AppDelegate.swift
+++ stockcomputer/stockcomputer/AppDelegate.swift
@@ -36,7 +36,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
         if #available(iOS 10.0, *) {
            // For iOS 10 display notification (sent via APNS)
            UNUserNotificationCenter.current().delegate = self
-
            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
            UNUserNotificationCenter.current().requestAuthorization(
              options: authOptions,
