69db587fa7c8271a8cb01f1644d0f84157f74bc9
Wei Wei <jheng.wei0919@gmail.com>
2019-10-30 18:26:25 +0800

    remove

--- stockcomputer/stockcomputer/AppDelegate.swift
+++ stockcomputer/stockcomputer/AppDelegate.swift
@@ -34,36 +34,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
         config.initializeSdk()
         Messaging.messaging().delegate = self
         if #available(iOS 10.0, *) {
-           // For iOS 10 display notification (sent via APNS)
-           UNUserNotificationCenter.current().delegate = self
-           let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-           UNUserNotificationCenter.current().requestAuthorization(
-             options: authOptions,
-             completionHandler: {_, _ in })
-         } else {
-           let settings: UIUserNotificationSettings =
-           UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-           application.registerUserNotificationSettings(settings)
-         }
-
-         application.registerForRemoteNotifications()
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
         
-
-//        .none
-//        .shake
-//        .screenshot
-//        .twoFingersSwipeLeft
-//        .rightEdgePan
-//        .floatingButton
+        application.registerForRemoteNotifications()
+        
+        
+        //        .none
+        //        .shake
+        //        .screenshot
+        //        .twoFingersSwipeLeft
+        //        .rightEdgePan
+        //        .floatingButton
         Instabug.start(withToken: "30dabcbd12350ce99d5037e8fb70858f", invocationEvents: [.shake, .screenshot,.twoFingersSwipeLeft,.rightEdgePan,.floatingButton])
         Instabug.setLocale(.chineseTaiwan)
-//        Instabug.welcomeMessageMode = IBGWelcomeMessageMode.live // For live users
-//        Instabug.welcomeMessageMode = IBGWelcomeMessageMode.disabled // Disable welcome message
-//        Instabug.showWelcomeMessage(with: IBGWelcomeMessageMode.live) // For live users
-
+        //        Instabug.welcomeMessageMode = IBGWelcomeMessageMode.live // For live users
+        //        Instabug.welcomeMessageMode = IBGWelcomeMessageMode.disabled // Disable welcome message
+        //        Instabug.showWelcomeMessage(with: IBGWelcomeMessageMode.live) // For live users
+        
         Replies.enabled = true
-
-
+        
+        
         return true
     }
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
@@ -91,9 +91,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
         let isInstabugNotification = Replies.didReceiveRemoteNotification(userInfo)
     }
     
-
-      func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                       fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         // If you are receiving a notification message while your app is in the background,
         // this callback will not be fired till the user taps on the notification launching the application.
         // TODO: Handle data of notification
@@ -101,90 +101,86 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
         // Messaging.messaging().appDidReceiveMessage(userInfo)
         // Print message ID.
         if let messageID = userInfo[gcmMessageIDKey] {
-          print("Message ID: \(messageID)")
+            print("Message ID: \(messageID)")
         }
-
+        
         // Print full message.
         print(userInfo)
-
+        
         completionHandler(UIBackgroundFetchResult.newData)
-      }
-      // [END receive_message]
-      func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    }
+    // [END receive_message]
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("Unable to register for remote notifications: \(error.localizedDescription)")
-      }
-
-      // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
-      // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
-      // the FCM registration token.
-      func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    }
+    
+    // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+    // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+    // the FCM registration token.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         print("APNs token retrieved: \(deviceToken)")
- Replies.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
-
+        Replies.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+        
         // With swizzling disabled you must set the APNs token here.
         // Messaging.messaging().apnsToken = deviceToken
-      }
     }
+}
 
-    // [START ios_10_message_handling]
-    @available(iOS 10, *)
-    extension AppDelegate : UNUserNotificationCenterDelegate {
-
-      // Receive displayed notifications for iOS 10 devices.
-      func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                  willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+// [START ios_10_message_handling]
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         let userInfo = notification.request.content.userInfo
-
+        
         // With swizzling disabled you must let Messaging know about the message, for Analytics
         // Messaging.messaging().appDidReceiveMessage(userInfo)
         // Print message ID.
         if let messageID = userInfo[gcmMessageIDKey] {
-          print("Message ID: \(messageID)")
+            print("Message ID: \(messageID)")
         }
-
+        
         // Print full message.
-        print(userInfo)
-
         // Change this to your preferred presentation option
         completionHandler([])
-      }
-
-      func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                  didReceive response: UNNotificationResponse,
-                                  withCompletionHandler completionHandler: @escaping () -> Void) {
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
         let userInfo = response.notification.request.content.userInfo
         // Print message ID.
         if let messageID = userInfo[gcmMessageIDKey] {
-          print("Message ID: \(messageID)")
+            print("Message ID: \(messageID)")
         }
-
+        
         // Print full message.
-        print(userInfo)
-
         completionHandler()
-      }
     }
-    // [END ios_10_message_handling]
+}
+// [END ios_10_message_handling]
 
-    extension AppDelegate : MessagingDelegate {
-      // [START refresh_token]
-      func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+extension AppDelegate : MessagingDelegate {
+    // [START refresh_token]
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
         print("Firebase registration token: \(fcmToken)")
         
         let dataDict:[String: String] = ["token": fcmToken]
         NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
         // TODO: If necessary send token to application server.
         // Note: This callback is fired at each app startup and whenever a new token is generated.
-      }
-      // [END refresh_token]
-      // [START ios_10_data_message]
-      // Receive data messages on iOS 10+ directly from FCM (bypassing APNs) when the app is in the foreground.
-      // To enable direct data messages, you can set Messaging.messaging().shouldEstablishDirectChannel to true.
-      func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+    }
+    // [END refresh_token]
+    // [START ios_10_data_message]
+    // Receive data messages on iOS 10+ directly from FCM (bypassing APNs) when the app is in the foreground.
+    // To enable direct data messages, you can set Messaging.messaging().shouldEstablishDirectChannel to true.
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
         print("Received data message: \(remoteMessage.appData)")
-      }
-      // [END ios_10_data_message]
+    }
+    // [END ios_10_data_message]
     
 }
 
