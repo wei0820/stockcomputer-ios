a4be7847c4102ca92f59495f43ddf23d49393c33
Wei Wei <jheng.wei0919@gmail.com>
2019-10-31 11:43:59 +0800

    remove code

--- stockcomputer/stockcomputer/FBLoginViewController.swift
+++ stockcomputer/stockcomputer/FBLoginViewController.swift
@@ -55,14 +55,7 @@ class FBLoginViewController: UIViewController{
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        fblogin.permissions = ["publicProfile,email"]
-        //        fblogin.delegate = self
-        //
-        //        //        第一次登入後可取得使用者token，後續即可直接登入
-        //        if (AccessToken.current) != nil{
-        //                        fetchProfile()
-        //
-        //        }
+        
         fetchProfile()
         // Do any additional setup after loading the view.
     }
