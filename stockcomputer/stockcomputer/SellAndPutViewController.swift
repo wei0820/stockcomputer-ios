343484a07771b426f6c4a3f8976a2f227c60777a
Wei Wei <jheng.wei0919@gmail.com>
2020-02-06 10:34:45 +0800

    add sell and put

---
+++ stockcomputer/stockcomputer/SellAndPutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SellAndPutViewController.swift
+//  stockcomputer
+//
+//  Created by oneplay on 2020/2/6.
+//  Copyright © 2020 jackpan. All rights reserved.
+//
+
+import UIKit
+
+class SellAndPutViewController: MGoogleADViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
