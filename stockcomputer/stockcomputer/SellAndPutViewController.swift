7b5e97fccd96fbc8c8cd42848890a0ed5641a5b1
Wei Wei <jheng.wei0919@gmail.com>
2020-02-07 14:48:05 +0800

    add fun

--- stockcomputer/stockcomputer/SellAndPutViewController.swift
+++ stockcomputer/stockcomputer/SellAndPutViewController.swift
@@ -7,38 +7,49 @@
 //
 
 import UIKit
-
+import Toaster
+import Kanna
+import Alamofire
+import JGProgressHUD
 class SellAndPutViewController: MGoogleADViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    var hud :JGProgressHUD?
     @IBOutlet weak var buypriceLabel: UILabel!
     @IBOutlet weak var selPriceLabel: UILabel!
     @IBOutlet weak var handPrcie: UILabel!
     @IBOutlet weak var buysellPrice: UILabel!
     @IBOutlet weak var TotalPrice: UILabel!
-    @IBAction func Cal_Button(_ sender: Any) {
-    }
     @IBOutlet weak var sellnumTF: UITextField!
-    
-    
     @IBOutlet weak var buynumTF: UITextField!
-    
     @IBOutlet weak var buyTF: UITextField!
-    
-
     @IBOutlet weak var sellTF: UITextField!
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func Cal_Button(_ sender: Any) {
+        if(buyTF.text!.isEmpty && buynumTF.text!.isEmpty &&
+            sellTF.text!.isEmpty && sellnumTF.text!.isEmpty){
+            setToast(s: "請輸入完整內容")
+            
+        }else{
+            //買進成交
+            var buypriceInt: Int = (Int(buynumTF.text!)! * Int(buyTF.text!)!) * 50
+            buypriceLabel.text = String(buypriceInt)
+            //賣出成交
+            var sellpriceInt: Int = (Int(sellnumTF.text!)! * Int(sellTF.text!)!) * 50
+            selPriceLabel.text = String(sellpriceInt)
+            //選擇權成交/20=稅金
+            var sellbuymoneyInt : Int = (((Int(buynumTF.text!)! * Int(buyTF.text!)!)/20) + ((Int(sellnumTF.text!)! * Int(sellTF.text!)!)/20))
+            buysellPrice.text = String(sellbuymoneyInt)
+            var handInt :Int =  (Int(buynumTF.text!)! +  Int(sellnumTF.text!)!) * 20
+            handPrcie.text = String (handInt)
+            var total : Int = sellpriceInt - buypriceInt - sellbuymoneyInt - handInt
+            TotalPrice.text = String(total)
+            
+            
+        }
     }
-    */
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
 }
