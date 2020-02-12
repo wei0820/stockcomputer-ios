911ad0e325f91797dbe44bd15cc643395ba71518
Wei Wei <jheng.wei0919@gmail.com>
2020-02-12 16:10:09 +0800

    firebaseManger swift add get  isLogin Member id

--- stockcomputer/stockcomputer/FirebaseManager.swift
+++ stockcomputer/stockcomputer/FirebaseManager.swift
@@ -12,75 +12,89 @@ import Firebase
 
 class FirebaseManager {
     static let userDefaults = UserDefaults.standard
-
-    static  func  addFireBaseDate(min: String , Type:String,place : String){
-           var  id = userDefaults.value(forKey: "token")
-           if ( id == nil){
-              id =  UiManager.getUUID()
-           }
-           let reference: DatabaseReference! = Database.database().reference().child("Reviews").child(id as! String)
-           let childRef = reference.childByAutoId() // 隨機生成的節點唯一識別碼，用來當儲存時的key值
-           let dateReviewReference = reference.child(DateManager.getDateString2())
-           // 新增節點資料
-               var dateReview: [String : AnyObject] = [String : AnyObject]()
-               dateReview["Id"] = id as AnyObject
-               dateReview["Minute"] = min  as AnyObject
-               dateReview["Type"] = Type as AnyObject
-                dateReview["Place"] = place as AnyObject
-            
-            dateReview["date"]  = DateManager.getDateforDate() as AnyObject
-           dateReview["createDate"] = DateManager.getDateString2() as AnyObject
-           dateReviewReference.updateChildValues(dateReview) { (err, ref) in
-               if err != nil{
-                   print("err： \(err!)")
-                   return
-               }
-               
-               print(ref.description())
-           }
-
-           
-       }
-  static  func SearchDatabase(){
-    var minarray =  Array<String>()
-    var dateArray = Array<String>()
-        var  id = FirebaseManager.userDefaults.value(forKey: "token")
-               if ( id == nil){
-                  id =  UiManager.getUUID()
-               }
-        // 查詢節點資料
-    Database.database().reference().child("Reviews").child(id as! String).observe(.childAdded, with: {
-            (snapshot) in
-        // childAdded逐筆呈現
-                 if let dictionaryData = snapshot.value as? [String: AnyObject]{
-                    print("Min",dictionaryData["Minute"])
-                    print("date",dictionaryData["date"])
-                    minarray.append(dictionaryData["Minute"] as! String)
-                    dateArray.append(dictionaryData["date"] as! String)
-
+    
+    func getMemberId () -> String{
+        let firebaseAuth = Auth.auth()
+        if firebaseAuth != nil {
+            if(!firebaseAuth.currentUser!.isAnonymous){
                 
-                 }
-        userDefaults.set(minarray, forKey: "minArray")
-        userDefaults.set(dateArray, forKey: "dateArray")
-
+                return  (Auth.auth().currentUser?.uid)!
+            }
+            
+            return ""
+            
+        }
+        return ""
+    }
+    
+    static  func  addFireBaseDate(userId : String ,min: String , Type:String,place : String){
+        var  id = userId
+        if ( id == nil){
+            id =  UiManager.getUUID()
+        }
+        let reference: DatabaseReference! = Database.database().reference().child("MemberList").child(id as! String)
+        let childRef = reference.childByAutoId() // 隨機生成的節點唯一識別碼，用來當儲存時的key值
+        let dateReviewReference = reference.child(DateManager.getDateString2())
+        // 新增節點資料
+        var dateReview: [String : AnyObject] = [String : AnyObject]()
+        dateReview["Id"] = id as AnyObject
+        dateReview["Minute"] = min  as AnyObject
+        dateReview["Type"] = Type as AnyObject
+        dateReview["Place"] = place as AnyObject
+        
+        dateReview["date"]  = DateManager.getDateforDate() as AnyObject
+        dateReview["createDate"] = DateManager.getDateString2() as AnyObject
+        dateReviewReference.updateChildValues(dateReview) { (err, ref) in
+            if err != nil{
+                print("err： \(err!)")
+                return
+            }
+            
+            print(ref.description())
+        }
+        
+        
+    }
+    static  func SearchDatabase(){
+        var minarray =  Array<String>()
+        var dateArray = Array<String>()
+        var  id = FirebaseManager.userDefaults.value(forKey: "token")
+        if ( id == nil){
+            id =  UiManager.getUUID()
+        }
+        // 查詢節點資料
+        Database.database().reference().child("MemberList").child(id as! String).observe(.childAdded, with: {
+            (snapshot) in
+            // childAdded逐筆呈現
+            if let dictionaryData = snapshot.value as? [String: AnyObject]{
+                print("Min",dictionaryData["Minute"])
+                print("date",dictionaryData["date"])
+                minarray.append(dictionaryData["Minute"] as! String)
+                dateArray.append(dictionaryData["date"] as! String)
+                
+                
+            }
+            userDefaults.set(minarray, forKey: "minArray")
+            userDefaults.set(dateArray, forKey: "dateArray")
+            
             
         }, withCancel: nil)
     }
     
-  static  func DeleteDatabase(){
+    static  func DeleteDatabase(){
         // 刪除節點資料
         
         var  id = FirebaseManager.userDefaults.value(forKey: "token")
-               if ( id == nil){
-                  id =  UiManager.getUUID()
-               }
-        Database.database().reference().child("Reviews").child(id as! String).removeValue { (error, ref) in
-                  if error != nil{
-                      print(error!)
-                      return
-                  }
-                  print("remove data success...")
+        if ( id == nil){
+            id =  UiManager.getUUID()
+        }
+        Database.database().reference().child("MemberList").child(id as! String).removeValue { (error, ref) in
+            if error != nil{
+                print(error!)
+                return
+            }
+            print("remove data success...")
             
-              }
+        }
     }
 }
