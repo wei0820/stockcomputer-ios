d78b7310898375edd89216f2c49a8d0feaaa8957
wei0820 <jheng.wei0919@gmail.com>
2020-02-29 18:48:50 +0800

    commit

--- stockcomputer/stockcomputer/AppDelegate.swift
+++ stockcomputer/stockcomputer/AppDelegate.swift
@@ -21,8 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
     
     var window: UIWindow?
     let gcmMessageIDKey = "gcm.message_id"
-    
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
