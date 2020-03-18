cafbc946f371a400f0f3941c58c251f2587004e0
Wei Wei <jheng.wei0919@gmail.com>
2020-03-18 16:41:14 +0800

    trust is done

--- stockcomputer/stockcomputer/GetStockPriceManger.swift
+++ stockcomputer/stockcomputer/GetStockPriceManger.swift
@@ -10,9 +10,9 @@ import Foundation
 import SwiftSoup
 class GetStockPriceManager{
     static let Url_a : String  = "https://histock.tw/stock/three.aspx?s=a"
-    let Url_b : String = "https://histock.tw/stock/three.aspx?s=b"
-    let Url_c :String = "https://histock.tw/stock/three.aspx?s=c"
-    let broker :String = "https://histock.tw/stock/broker8.aspx"
+    static let Url_b : String = "https://histock.tw/stock/three.aspx?s=b"
+    static let Url_c :String = "https://histock.tw/stock/three.aspx?s=c"
+   static  let broker :String = "https://histock.tw/stock/broker8.aspx"
     static let Url :String = "https://histock.tw/app/table.aspx"
     static var document: Document = Document.init("")
     static var array = Array<StockData>()
@@ -108,6 +108,89 @@ class GetStockPriceManager{
     static  func getForeigninvestment_2() -> Array<String> {
         array_foreigninvestment.removeAll()
         guard let url = URL(string: Url_a ?? "") else {
+            // an error occurred
+            return array_foreigninvestment
+        }
+        
+        do {
+            
+            // content of url
+            let html = try String.init(contentsOf: url)
+            
+            // parse it into a Document
+            document = try SwiftSoup.parse(html)
+            // parse css query
+            do {
+                
+                //empty old items
+                // firn css selector
+                let elements: Elements = try document.select("div.tb-outline.outline2>ul.stock-list>li" ?? "")
+                //transform it into a local object (Item)
+                for th in try elements.select("span.w58.name") {
+                    let n = try th.text()
+                    if(!n.isEmpty){
+                        print("jack",n)
+                        array_foreigninvestment.append(n)
+
+                    }
+
+                }
+                
+            } catch let error {
+            }
+            
+            
+        } catch let error {
+            // an error occurred
+        }
+        return array_foreigninvestment
+    }
+    
+    
+    static  func getTust() -> Array<String> {
+        array_foreigninvestment.removeAll()
+        guard let url = URL(string: Url_b ?? "") else {
+            // an error occurred
+            return array_foreigninvestment
+        }
+        
+        do {
+            
+            // content of url
+            let html = try String.init(contentsOf: url)
+            
+            // parse it into a Document
+            document = try SwiftSoup.parse(html)
+            // parse css query
+            do {
+                
+                //empty old items
+                // firn css selector
+                let elements: Elements = try document.select("div.tb-outline.outline1>ul.stock-list>li" ?? "")
+                //transform it into a local object (Item)
+                for th in try elements.select("span.w58.name") {
+                    let n = try th.text()
+                    if(!n.isEmpty){
+                        print("jack",n)
+                        array_foreigninvestment.append(n)
+
+                    }
+
+                }
+                
+            } catch let error {
+            }
+            
+            
+        } catch let error {
+            // an error occurred
+        }
+        return array_foreigninvestment
+    }
+    
+    static  func getTust_2() -> Array<String> {
+        array_foreigninvestment.removeAll()
+        guard let url = URL(string: Url_b ?? "") else {
             // an error occurred
             return array_foreigninvestment
         }
