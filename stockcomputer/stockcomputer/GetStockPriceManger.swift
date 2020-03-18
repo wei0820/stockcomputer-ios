50a5f4a4e73d08202e367fc8ad7370d0aa572d1d
Wei Wei <jheng.wei0919@gmail.com>
2020-03-18 16:13:29 +0800

    add view and function

--- stockcomputer/stockcomputer/GetStockPriceManger.swift
+++ stockcomputer/stockcomputer/GetStockPriceManger.swift
@@ -9,15 +9,16 @@
 import Foundation
 import SwiftSoup
 class GetStockPriceManager{
-    let Url_a : String  = "https://histock.tw/stock/three.aspx?s=a"
+    static let Url_a : String  = "https://histock.tw/stock/three.aspx?s=a"
     let Url_b : String = "https://histock.tw/stock/three.aspx?s=b"
     let Url_c :String = "https://histock.tw/stock/three.aspx?s=c"
     let broker :String = "https://histock.tw/stock/broker8.aspx"
     static let Url :String = "https://histock.tw/app/table.aspx"
     static var document: Document = Document.init("")
     static var array = Array<StockData>()
+    static var array_foreigninvestment = Array<String>()
     static  func get() -> Array<StockData> {
-        array.removeAll()
+        array_foreigninvestment.removeAll()
         guard let url = URL(string: Url ?? "") else {
             // an error occurred
             return array
@@ -42,7 +43,7 @@ class GetStockPriceManager{
                     
                     let th = try th.text()
                     if(!th.isEmpty){
-                       
+                        
                         stock.setTitle(s:  String(th.split(separator: " ")[0]))
                         stock.setDetail(d: String(th.split(separator: " ")[1]))
                         array.append(stock)
@@ -50,7 +51,7 @@ class GetStockPriceManager{
                     }
                     
                 }
-                                
+                
             } catch let error {
             }
             
@@ -59,5 +60,89 @@ class GetStockPriceManager{
             // an error occurred
         }
         return array
+    }
+    
+    
+    
+    static  func getForeigninvestment() -> Array<String> {
+        array_foreigninvestment.removeAll()
+        guard let url = URL(string: Url_a ?? "") else {
+            // an error occurred
+            return array_foreigninvestment
+        }
+        
+        do {
+            
+            // content of url
+            let html = try String.init(contentsOf: url)
+            
+            // parse it into a Document
+            document = try SwiftSoup.parse(html)
+            // parse css query
+            do {
+                
+                //empty old items
+                // firn css selector
+                let elements: Elements = try document.select("div.tb-outline.outline1>ul.stock-list>li" ?? "")
+                //transform it into a local object (Item)
+                for th in try elements.select("span.w58.name") {
+                    let n = try th.text()
+                    if(!n.isEmpty){
+                        print("jack",n)
+                        array_foreigninvestment.append(n)
+
+                    }
+
+                }
+                
+            } catch let error {
+            }
+            
+            
+        } catch let error {
+            // an error occurred
+        }
+        return array_foreigninvestment
+    }
+    
+    static  func getForeigninvestment_2() -> Array<String> {
+        array_foreigninvestment.removeAll()
+        guard let url = URL(string: Url_a ?? "") else {
+            // an error occurred
+            return array_foreigninvestment
+        }
+        
+        do {
+            
+            // content of url
+            let html = try String.init(contentsOf: url)
+            
+            // parse it into a Document
+            document = try SwiftSoup.parse(html)
+            // parse css query
+            do {
+                
+                //empty old items
+                // firn css selector
+                let elements: Elements = try document.select("div.tb-outline.outline2>ul.stock-list>li" ?? "")
+                //transform it into a local object (Item)
+                for th in try elements.select("span.w58.name") {
+                    let n = try th.text()
+                    if(!n.isEmpty){
+                        print("jack",n)
+                        array_foreigninvestment.append(n)
+
+                    }
+
+                }
+                
+            } catch let error {
+            }
+            
+            
+        } catch let error {
+            // an error occurred
+        }
+        return array_foreigninvestment
     }
 }
