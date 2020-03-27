75c51b6f69b9a526863b177064d07a81ff864ccc
Wei Wei <jheng.wei0919@gmail.com>
2020-03-27 14:31:13 +0800

    close instabug screenshot

--- stockcomputer/stockcomputer/AppDelegate.swift
+++ stockcomputer/stockcomputer/AppDelegate.swift
@@ -58,7 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
         //        .twoFingersSwipeLeft
         //        .rightEdgePan
         //        .floatingButton
-        Instabug.start(withToken: "30dabcbd12350ce99d5037e8fb70858f", invocationEvents: [.shake, .screenshot,.twoFingersSwipeLeft,.rightEdgePan,.floatingButton])
+        Instabug.start(withToken: "30dabcbd12350ce99d5037e8fb70858f", invocationEvents: [.shake, .twoFingersSwipeLeft,.rightEdgePan,.floatingButton])
         Instabug.setLocale(.chineseTaiwan)
         //        Instabug.welcomeMessageMode = IBGWelcomeMessageMode.live // For live users
         //        Instabug.welcomeMessageMode = IBGWelcomeMessageMode.disabled // Disable welcome message
