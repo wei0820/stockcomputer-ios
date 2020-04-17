bce6e5ea3eb6bd339a44493266ce1f6c4606f57a
Wei Wei <jheng.wei0919@gmail.com>
2020-04-17 17:39:17 +0800

    commit

---
+++ stockcomputer/stockcomputer/SelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectViewController.swift
+//  stockcomputer
+//
+//  Created by oneplay on 2020/4/16.
+//  Copyright © 2020 jackpan. All rights reserved.
+//
+
+import UIKit
+
+class SelectViewController: MGoogleADViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
