35b87e5ac9dc49927423fd9e0ec99a8cc0b97e50
Wei Wei <jheng.wei0919@gmail.com>
2020-05-20 15:20:55 +0800

    add function

--- stockcomputer/stockcomputer/Financing ViewController.swift
+++ stockcomputer/stockcomputer/Financing ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class Financing_ViewController: MGoogleADViewController {
-
+    let fullScreenSize = UIScreen.main.bounds.size
+       var formatter: DateFormatter! = nil
+    var startDateStr :Date? = nil
+       var EndDateStr :Date? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
+        sellNum.text = "1000"
+        buyNum.text = "1000"
+        setDatePickerView()
+        setUITextField()
 
         // Do any additional setup after loading the view.
     }
     @IBAction func cal(_ sender: Any) {
+        
+        if(buyMoney.text!.isEmpty && sellMoney.text!.isEmpty && loanMOney.text!.isEmpty){
+            setToast(s: "請輸入數值")
+            
+        }else{
+            var buyMoneyDoube :Double = Double(buyMoney.text!)!
+            var sellMoneyDouble : Double = Double(sellMoney.text!)!
+            var sellNumInt : Int =  Int(sellNum.text!)!
+            var buyNumInt  : Int =  Int(buyNum.text!)!
+            var loandMoneyDoule : Double =  0.1 * Double(loanMOney.text!)!
+            // 買入 應付金額
+            // 融資金
+            var loanMoneyInt :Double = buyMoneyDoube * Double(sellNumInt) * loandMoneyDoule
+            var handPrice : Double = buyMoneyDoube * Double(buyNumInt) * 0.001425
+            var buyMoneyInt :Int = Int( buyMoneyDoube * Double(buyNumInt))  - Int(loanMoneyInt) + Int(handPrice)
+            print("Jack",buyMoneyInt)
+//
+            
+        }
+        
+        
+        
+        
+        
     }
+    
+    
+    func setUITextField(){
+         buyNum.keyboardType = .decimalPad
+         sellNum.keyboardType = .decimalPad
+         buyMoney.keyboardType = .decimalPad
+         sellMoney.keyboardType = .decimalPad
+        loanMOney.keyboardType = .decimalPad
+         buyNum.resignFirstResponder()
+         sellNum.resignFirstResponder()
+         buyMoney.resignFirstResponder()
+         sellMoney.resignFirstResponder()
+        loanMOney.resignFirstResponder()
+        loanMOney.placeholder = "輸入成數(ex:6成 輸入 6)"
+         
+     }
     @IBOutlet weak var sellMoney: UITextField!
     @IBOutlet weak var buyMoney: UITextField!
     @IBOutlet weak var endTime: UITextField!
+    @IBOutlet weak var loanMOney: UITextField!
     
     @IBOutlet weak var sellNum: UITextField!
     @IBOutlet weak var buyNum: UITextField!
@@ -30,23 +78,103 @@ class Financing_ViewController: MGoogleADViewController {
     
     @IBOutlet weak var label_3: UILabel!
     
+    @IBOutlet weak var label_6: UILabel!
     
     @IBOutlet weak var label_4: UILabel!
     
     @IBOutlet weak var label_5: UILabel!
     
-    /*
-    // MARK: - Navigation
-     
-     
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func closeview(_ sender: Any) {
         dissmissView()
     }
+    
+    
+    func setDatePickerView(){
+           formatter = DateFormatter()
+           formatter.dateFormat = "yyyy/MM/dd"
+           
+           // 建立一個 UIDatePicker
+           let myDatePicker = UIDatePicker()
+           let myDatePicker2 = UIDatePicker()
+           
+           
+           // 設置 UIDatePicker 格式
+           myDatePicker.datePickerMode = .date
+           myDatePicker2.datePickerMode = .date
+           
+           
+           // 設置 UIDatePicker 顯示的語言環境
+           myDatePicker.locale = Locale(identifier: "zh_TW")
+           myDatePicker2.locale = Locale(identifier: "zh_TW")
+           
+           
+           // 設置 UIDatePicker 預設日期為現在日期
+           myDatePicker.date = Date()
+           myDatePicker2.date = Date()
+           
+           
+           // 設置 UIDatePicker 改變日期時會執行動作的方法
+           myDatePicker.addTarget(self, action: #selector(MarginController.datePickerChanged), for: .valueChanged)
+           
+           myDatePicker2.addTarget(self, action: #selector(MarginController.datePickerChanged), for: .valueChanged)
+           
+           
+           myDatePicker.tag = 1
+           myDatePicker2.tag = 2
+           // 將 UITextField 原先鍵盤的視圖更換成 UIDatePicker
+           startTime.inputView = myDatePicker
+           endTime.inputView = myDatePicker2
+           
+           // 設置 UITextField 預設的內容
+           startTime.text = formatter.string(from: myDatePicker.date)
+           endTime.text = formatter.string(from: myDatePicker2.date)
+           startDateStr =  myDatePicker.date
+           EndDateStr = myDatePicker2.date
+           
+           // 設置 UITextField 的 tag 以利後續使用
+           startTime.tag = 200
+           self.view.addSubview(startTime)
+           endTime.tag = 300
+           self.view.addSubview(endTime)
+           // 增加一個觸控事件
+           let tap = UITapGestureRecognizer(target: self, action: #selector(MarginController.hideKeyboard(tapG:)))
+           tap.cancelsTouchesInView = false
+           // 加在最基底的 self.view 上
+           self.view.addGestureRecognizer(tap)
+           
+           
+       }
+       
+       
+       
+       // UIPickerViewDataSource 必須實作的方法：UIPickerView 有幾列可以選擇
+       func numberOfComponents(in pickerView: UIPickerView) -> Int {
+           return 1
+       }
+       
+       
+       // UIDatePicker 改變選擇時執行的動作
+       @objc func datePickerChanged(datePicker:UIDatePicker) {
+           // 依據元件的 tag 取得 UITextFiel
+           let myTextField = self.view?.viewWithTag(200) as? UITextField
+           let myTextField2 = self.view?.viewWithTag(300) as? UITextField
+           
+           if(datePicker.tag == 1){
+               myTextField?.text = formatter.string(from: datePicker.date)
+               startDateStr =  datePicker.date
+               
+           }else{
+               myTextField2?.text = formatter.string(from: datePicker.date)
+               EndDateStr = datePicker.date
+               
+               
+           }
+       }
+       
+       // 按空白處會隱藏編輯狀態
+       @objc func hideKeyboard(tapG:UITapGestureRecognizer){
+           self.view.endEditing(true)
+       }
+       
     
 }
