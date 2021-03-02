ef1ea6469e52f8076ed80c8d5bf6b884a1478e4d
wei0820 <jheng.wei0919@gmail.com>
2021-03-02 20:38:19 +0800

    add new viewcintroller

---
+++ stockcomputer/stockcomputer/SmallFuturesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SmallFuturesViewController.swift
+//  stockcomputer
+//
+//  Created by  JackPan on 2021/3/2.
+//  Copyright © 2021 jackpan. All rights reserved.
+//
+
+import UIKit
+
+class SmallFuturesViewController: MGoogleADViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
