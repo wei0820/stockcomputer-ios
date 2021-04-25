03b4fdb2221d961433c40f8b2999c473f98d4ca4
wei0820 <jheng.wei0919@gmail.com>
2021-04-25 11:41:14 +0800

    add fun

---
+++ stockcomputer/stockcomputer/SelectStockPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SelectStockPageViewController.swift
+//  stockcomputer
+//
+//  Created by  JackPan on 2021/4/25.
+//  Copyright © 2021 jackpan. All rights reserved.
+//
+
+import UIKit
+
+class SelectStockPageViewController: MGoogleADViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
