0cdda2963f43a6ea7584800437669e3384ba5806
wei0820 <jheng.wei0919@gmail.com>
2021-05-29 15:14:25 +0800

    add StockDividendlistData

---
+++ stockcomputer/stockcomputer/Data/StockDividendlistData.swift
@@ -0,0 +1,13 @@
+//
+//  StockDividendlistData.swift
+//  stockcomputer
+//
+//  Created by  JackPan on 2021/5/29.
+//  Copyright © 2021 jackpan. All rights reserved.
+//
+
+import Foundation
+
+class StockDividendlistData{
+    
+}
